fe49c0e5a8d8e27afce0de7a4da1246750fd7422
sara <sara.tankard@gmail.com>
2021-10-12 13:09:10 -0400

    Fixing relative urls

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,13 +20,13 @@ const Home: NextPage = () => {
 
       <footer className={styles.footer}>
         <a href="https://github.com/gecko25" target="_blank" rel="noreferrer">
-          <Image src="github.png" width={30} alt="Sara's Github"/>
+          <Image src="/github.png" width={40} height={40} alt="Sara's Github"/>
         </a>
         <a href="https://www.linkedin.com/in/saratankard/" target="_blank" rel="noreferrer">
-          <Image src="linkedin.png" width={30} alt="Sara's LinkedIn" />
+          <Image src="/linkedin.png" width={40} height={40}  alt="Sara's LinkedIn" />
         </a>
         <a href="https://docs.google.com/document/d/1LMMeQQR8oJd_hs37OI3r07F-oNvlfHd9be4ScKTSgPk/edit?usp=sharing" target="_blank" rel="noreferrer">
-          <Image src="resume.png" width={30} alt="Sara's Resume" />
+          <Image src="/resume.png" width={40} height={40} alt="Sara's Resume" />
         </a>
         
       </footer>
