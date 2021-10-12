81c68d2030788a8ff96152af7cf4aaa26155bd56
sara <sara.tankard@gmail.com>
2021-10-12 15:50:56 -0400

    Adding sass support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require("path");
 
 module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "variables.scss";`,
+  },
   webpack(config) {
     config.plugins.push(
       new ESLintPlugin({
